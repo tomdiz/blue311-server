a963d1a7c627b2f7c135b3ddd3b8a60c0443e2b8
Tom DiZoglio <tom@peerspace.com>
2015-04-18 01:08:44 +0000

    added routes

---
+++ routes/comments.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Comment = require('../models/Comment.js');
+
+/* GET /comments for location id. */
+router.get('/', function(req, res, next) {
+
+/*
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+*/
+  //clean these variables:
+  var query = req.query;
+  var location_id = Number(query.location_id);
+  var limit = (typeof(query.limit) !== "undefined") ? query.limit : 40;
+  if(!(Number(query.location_id))
+  {
+    res.send(500, {http_status:400,error_msg: "this endpoint requires a location_id: location_id\na query 'limit' parameter can be optionally specified as well."});
+    return console.error('could not connect to the database', err);
+  }
+  db[collection_name].find( {"location_id" : location_id}).limit(limit).toArray(function(err,rows) {
+    if(err) {
+      res.send(500, {http_status:500,error_msg: err})
+      return console.error('error running query', err);
+    }
+    res.send(rows);
+    return rows;
+  });
+
+});
+
+/* POST /comments */
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /comments/id */
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /comments/:id */
+router.put('/:id', function(req, res, next) {
+//  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+  Comment.findByIdAndUpdate(req.params.id, {$inc: {rating_up:1}}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /comments/:id */
+router.delete('/:id', function(req, res, next) {
+  //Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+  Comment.findByIdAndUpdate(req.params.id, {$inc: {rating_down:1}}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
---
+++ routes/maplocations.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Comment = require('../models/Maplocation.js');
+
+/* GET /maplocations. */
+router.get('/', function(req, res, next) {
+  Maplocation.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+/* POST /maplocations */
+router.post('/', function(req, res, next) {
+  Maplocation.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /maplocations/within */
+router.get('/within', function(req, res, next) {
+  //clean these variables:
+  var query = req.query;
+  var lat = Number(query.lat),
+      lon = Number(query.radius),
+      radius = Number(query.lon),
+  var limit = (typeof(query.limit) !== "undefined") ? query.limit : 40;
+  if(!(Number(query.lat) 
+    && Number(query.lon) 
+    && Number(query.radius) 
+    && Number(limit)))
+  {
+    res.send(500, {http_status:400,error_msg: "this endpoint requires a lat, long coordinate and radius: lat lon radius\na query 'limit' parameter can be optionally specified as well."});
+    return console.error('could not connect to the database', err);
+  }
+  db[collection_name].find( {"pos" : {'$geoWithin': { '$centerSphere': [[lon,lat],radius]}}}).limit(limit).toArray(function(err,rows) {
+    if(err) {
+      res.send(500, {http_status:500,error_msg: err})
+      return console.error('error running query', err);
+    }
+    res.send(rows);
+    return rows;
+  });
+});
+
+/* PUT /maplocations/update/:id */
+router.put('/:id', function(req, res, next) {
+  Maplocation.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /maplocations/:id */
+router.put('/:id', function(req, res, next) {
+  Maplocation.findByIdAndUpdate(req.params.id, {inUse:true}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /maplocations/:id */
+router.delete('/:id', function(req, res, next) {
+  Maplocation.findByIdAndUpdate(req.params.id, {inUse:false}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
