83e4e06dda2a2f57bed8d196173c701ae60c66dd
Tom DiZoglio <tomdiz@yahoo.com>
2015-04-18 03:56:05 +0000

    forgot to add new files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,7 +7,6 @@ var cc = require('config-multipaas');
 var config = cc();
 
 var routes = require('./routes/index');
-//var todos = require('./routes/todos');
 
 var comments = require('./routes/comments');
 var maplocations = require('./routes/maplocations');
@@ -29,16 +27,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', routes);
-//app.use('/todos', todos);
 app.use('/v1/comments', comments);
 app.use('/v1/maplocations', maplocations);
 
